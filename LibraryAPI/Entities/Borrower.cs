ed7b9bc6ac985af29b3f34d09e57a574fc5a1b1a
Marcin Smolen <smolen.m@e80group.com>
2023-07-25 21:03:32 +0200

    entities

---
+++ LibraryAPI/Entities/Borrower.cs
@@ -0,0 +1,11 @@
+﻿namespace LibraryAPI.Entities
+{
+    public class Borrower
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public int[] BorrowedBooksIds { get; set; }
+        public virtual Book[] Books { get; set; }
+    }
+}
