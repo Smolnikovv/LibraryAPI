a92ba757c738fab7fe9812116b5f30f36668d8b0
Marcin Smolen <smolen.m@e80group.com>
2023-07-26 17:41:52 +0200

    database context and fixing entities

---
+++ LibraryAPI/Configs/DatabaseContext.cs
@@ -0,0 +1,62 @@
+﻿using LibraryAPI.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryAPI.Configs
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) :base(options) 
+        { }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Borrower> Borrowers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            PropGenres(modelBuilder);
+            PropAuthors(modelBuilder);
+            PropBooks(modelBuilder);
+            PropBorrowers(modelBuilder);
+        }
+        private void PropGenres(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Genre>()
+                .Property(x => x.Description)
+                .IsRequired();
+        }
+        private void PropAuthors(ModelBuilder modelBuilder) 
+        { 
+            modelBuilder.Entity<Author>()
+                .Property(x => x.Name)
+                .IsRequired();
+            modelBuilder.Entity<Author>()
+                .Property(x => x.Surname)
+                .IsRequired();
+        }
+        private void PropBooks(ModelBuilder modelBuilder) 
+        {
+            modelBuilder.Entity<Book>()
+                .Property(x => x.Name)
+                .IsRequired();
+            modelBuilder.Entity<Book>()
+                .Property(x => x.AuthorId)
+                .IsRequired();
+            modelBuilder.Entity<Book>()
+                .Property(x => x.GenreId)
+                .IsRequired();
+        }
+        private void PropBorrowers(ModelBuilder modelBuilder) 
+        { 
+            modelBuilder.Entity<Borrower>()
+                .Property(x => x.Name)
+                .IsRequired();
+            modelBuilder.Entity<Borrower>()
+                .Property(x => x.DateOfBirth)
+                .IsRequired();
+            modelBuilder.Entity<Borrower>()
+                .Property(x => x.Surname)
+                .IsRequired();
+        }
+    }
+}
--- LibraryAPI/Entities/Borrower.cs
+++ LibraryAPI/Entities/Borrower.cs
@@ -4,8 +4,17 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Surname { get; set; }
         public DateTime DateOfBirth { get; set; }
-        public int[] BorrowedBooksIds { get; set; }
-        public virtual Book[] Books { get; set; }
+        public int? FirstBookId { get; set; }
+        public int? SecondBookId { get; set; }
+        public int? ThirdBookId { get; set; }
+        public int? FourthBookId { get; set; }
+        public int? FifthBookId { get; set; }
+        public virtual Book? FirtBook { get; set; }
+        public virtual Book? SecondBook { get; set; }
+        public virtual Book? ThirdBook { get; set; }
+        public virtual Book? FourthBook { get; set; }
+        public virtual Book? FifthBok { get; set; }
     }
 }
